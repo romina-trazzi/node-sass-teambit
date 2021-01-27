e84f35832d3611a374b7e3ff70b40a3734059d3f
Romina Trazzi <romina.trazzi@posta.it>
2021-01-27 13:37:46 +0100

    Commit refactoring jumbotron

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,3 @@ mix
 .js('src/main.js', 'js') // Prende il file dalla cartella search, lo compila e mette il nuovo in dist\js
 .sass('src/style.scss', 'css') // Prende il file dalla cartella search, lo compila e mette il nuovo in dist\css
 .setPublicPath('dist'); // Stabilisce il nome della cartella principale dei file compilati
-
